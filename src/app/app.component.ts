f6167a0d4e3ca731c5c97d2b2b7ca1635018eb71
Kimserey <kimserey.lam@gmail.com>
2019-01-20 09:36:41 +0000

    more

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'my-first-application';
+  name: string;
+  
+  constructor(private http: HttpClient) { }
 
   click() {
     $('#myButton').button('toggle');
+  }
+  
+  ngOnInit() {
+    this.http.get<string>('/api')
+      .subscribe(name => {
+        this.name = name;
+      });
   }
 }
